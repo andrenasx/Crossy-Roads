e0ced58518de983a56f1731828d56f0a5765f2d8
anatcruz <anateresacruz2000@gmail.com>
2020-04-18 17:09:46 +0100

    Added coin class

---
+++ src/main/java/Model/Coin.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Coin extends Element {
+    private int value;
+    public Coin(int x, int y, int value) {
+        super(x, y);
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
