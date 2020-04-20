532e29a109a5d9bccf8ff63adda724c05fc9ca09
anatcruz <anateresacruz2000@gmail.com>
2020-04-20 16:51:47 +0100

    Stuff

--- src/main/java/Model/GameMapCreator.java
+++ src/main/java/Model/GameMapCreator.java
@@ -10,7 +10,7 @@ public class GameMapCreator {
         gameMap.addElement(car);
         Truck truck = new Truck(10, 20, "left");
         gameMap.addElement(truck);
-        Coin coin = new Coin(30,30,1);
+        Coin coin = new Coin(30,10,1);
         Coin coin2 = new Coin(20, 15, 3);
         gameMap.addElement(coin);
         gameMap.addElement(coin2);
