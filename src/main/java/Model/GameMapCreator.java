46e30c0d3728636c72edd4719429f80fb37bbae3
andrenasx <andre2000.an@gmail.com>
2020-04-20 14:52:42 +0100

    Added coins to map, to test colisions

--- src/main/java/Model/GameMapCreator.java
+++ src/main/java/Model/GameMapCreator.java
@@ -10,6 +10,10 @@ public class GameMapCreator {
         gameMap.addElement(car);
         Truck truck = new Truck(10, 20, "left");
         gameMap.addElement(truck);
+        Coin coin = new Coin(30,30,1);
+        Coin coin2 = new Coin(20, 15, 3);
+        gameMap.addElement(coin);
+        gameMap.addElement(coin2);
         return gameMap;
     }
 
