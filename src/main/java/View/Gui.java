8bc26448bdcc33284e528cf3ad208b4f792a8347
anatcruz <anateresacruz2000@gmail.com>
2020-04-20 16:50:54 +0100

    Changed the color and size of vehicles

--- src/main/java/View/Gui.java
+++ src/main/java/View/Gui.java
@@ -48,9 +48,9 @@ public class Gui {
 
     private void drawElement(Element element) {
         if (element instanceof Chicken) drawCharacter(element.getPosition(), "O", "#FFFFFF");
-        if (element instanceof Car) drawCharacter(element.getPosition(), "C", "#FFFFFF");
-        if (element instanceof Truck) drawCharacter(element.getPosition(), "TT", "#FFFFFF");
-        if (element instanceof Coin) drawCharacter(element.getPosition(), "O", "#FFFF00");
+        if (element instanceof Car) drawCharacter(element.getPosition(), "CC", "#FF0000");
+        if (element instanceof Truck) drawCharacter(element.getPosition(), "TTTTT", "#0000FF");
+        if (element instanceof Coin) drawCharacter(element.getPosition(), "S", "#FFFF00");
     }
 
     private void drawCharacter(Position position, String character, String color) {
