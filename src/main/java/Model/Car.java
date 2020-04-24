0c1c623b4f2561612f88073d9a8b4c56a3664fe1
Pedro Coelho <coelho.pedromiguel@gmail.com>
2020-04-24 17:07:03 +0100

    Fixed variable name

--- src/main/java/Model/Car.java
+++ src/main/java/Model/Car.java
@@ -3,7 +3,7 @@ package Model;
 public class Car extends Vehicle {
     public Car(int x, int y, String direction) {
         super(x, y, direction);
-        this.lenght=2;
+        this.length=2;
         setCharacter("C");
         setColor("#FF0000");
     }
--- src/main/java/Model/Truck.java
+++ src/main/java/Model/Truck.java
@@ -3,7 +3,7 @@ package Model;
 public class Truck extends Vehicle {
     public Truck(int x, int y, String direction) {
         super(x, y, direction);
-        this.lenght=4;
+        this.length=4;
         setCharacter("T");
         setColor("#0000FF");
     }
