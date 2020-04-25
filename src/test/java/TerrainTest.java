c4e1b8653d8a99b957f9f264d0329abf673df2bb
anatcruz <anateresacruz2000@gmail.com>
2020-04-25 19:01:51 +0100

    Added test to verify if the readFile function is opening the files correctly and assembling the strings

---
+++ src/test/java/TerrainTest.java
@@ -0,0 +1,11 @@
+import model.Terrain;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TerrainTest {
+    @Test
+    public void readFileTest(){
+        Terrain terrain = new Terrain("test.txt", "test");
+        assertEquals("Test", terrain.getTerrainStrings());
+    }
+}
