db2c1413bc2bccbe8bad9820b6cac8c60499fcc4
andrenasx <andre2000.an@gmail.com>
2020-04-29 15:25:24 +0100

    Created VehicleController and moving vehicles

---
+++ src/main/java/controller/VehicleController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import model.GameMap;
+import model.Vehicle;
+
+import java.util.List;
+
+public class VehicleController {
+    private GameMap gameMap;
+    private long startTime;
+
+
+    public VehicleController(GameMap gameMap) {
+        this.gameMap = gameMap;
+        this.startTime = System.currentTimeMillis();
+    }
+
+    private void moveVehicles() {
+        List<Vehicle> vehicles = gameMap.getVehicles();
+        for (Vehicle vehicle: vehicles){
+            if(vehicle.getDirection().equals("left"))
+                vehicle.setPosition(vehicle.getPosition().left());
+            else
+                vehicle.setPosition(vehicle.getPosition().right());
+        }
+        gameMap.notifyObservers();
+    }
+
+    public void start() {
+        while(!gameMap.isGameFinished()){
+            if ((System.currentTimeMillis() - startTime) % 1000 == 0)
+                moveVehicles();
+        }
+    }
+
+}
