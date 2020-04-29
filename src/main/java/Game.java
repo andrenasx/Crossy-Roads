da8d7e6295b89f4bf6c4caeeb5663453a9564066
andrenasx <andre2000.an@gmail.com>
2020-04-29 15:24:49 +0100

    Removed while cycle

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -9,14 +9,12 @@ import java.io.IOException;
 public class Game implements GameMapObserver {
     private GameMap map;
     private Gui gui;
-    private long startTime;
 
     public static void main(String[] args) throws IOException {
         new Game().start();
     }
 
     private void start() throws IOException {
-        startTime = System.currentTimeMillis();
         GameMapCreator creator = new GameMapCreator();
         map = creator.createGameMap(40,35);
         map.addObserver(this);
@@ -24,15 +22,9 @@ public class Game implements GameMapObserver {
         gui = new Gui(map);
         gui.draw();
 
-        GameController commands = new GameController(gui, map);
-        //commands.execute();
+        GameController controller = new GameController(gui, map);
 
-
-        while(!map.isGameFinished()){
-            commands.execute();
-            if ((System.currentTimeMillis() - startTime) % 1000 == 0)
-                map.moveVehicles();
-        }
+        controller.start();
     }
 
     @Override
