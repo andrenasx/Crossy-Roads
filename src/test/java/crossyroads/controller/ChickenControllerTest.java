ee50e8b09b8ef3779ded5a4f6e908f766466a91a
anatcruz <anateresacruz2000@gmail.com>
2020-05-02 18:04:15 +0100

    Added tests

--- src/test/java/crossyroads/controller/ChickenControllerTest.java
+++ src/test/java/crossyroads/controller/ChickenControllerTest.java
@@ -1,19 +1,20 @@
 package crossyroads.controller;
 
-import crossyroads.model.GameMap;
-import crossyroads.model.Position;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import crossyroads.model.*;
 import org.junit.Test;
 import crossyroads.view.Gui;
+import org.mockito.Mockito;
 
 import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class ChickenControllerTest {
-    /*@Test
-    public void chickenStaysInScreenTest() throws IOException {
+    @Test
+    public void chickenStaysInScreenTest(){
         GameMap map = new GameMap(40, 35);
-        Gui gui = new Gui(map);
+        Gui gui = Mockito.mock(Gui.class);
         ChickenController chickenController = new ChickenController(gui, map);
         Position position1 = new Position(1, 2);
         Position position2 = new Position(46, -30);
@@ -26,37 +27,118 @@ public class ChickenControllerTest {
     }
 
     @Test
-    public void moveChickenTest() throws IOException {
+    public void moveChickenTest(){
         GameMap map = new GameMap(40, 35);
-        Gui gui = new Gui(map);
+        Gui gui = Mockito.mock(Gui.class);
         ChickenController chickenController = new ChickenController(gui, map);
+        Chicken chicken = map.getChicken();
         Position position1 = new Position(10, 20);
         Position position2 = new Position(50, 15);
         chickenController.moveChicken(position1);
-        assertEquals(map.getChickenPosition(), position1);
-        Position position = map.getChickenPosition();
+        assertEquals(chicken.getPosition(), position1);
+        Position position = chicken.getPosition();
         chickenController.moveChicken(position2);
-        assertEquals(map.getChickenPosition(), position);
-
+        assertEquals(chicken.getPosition(), position);
     }
 
     @Test
-    public void executeTest() throws IOException {
+    public void UpTest() throws IOException {
         GameMap map = new GameMap(40, 35);
-        Gui gui = new Gui(map);
+        Gui gui = Mockito.mock(Gui.class);
         ChickenController chickenController = new ChickenController(gui, map);
-        Position position1 = map.getChickenPosition();
-        chickenController.execute(Gui.COMMAND.UP);
-        assertEquals(map.getChickenPosition(), new Position(position1.getX(), position1.getY()-1));
-        Position position2 = map.getChickenPosition();
-        chickenController.execute(Gui.COMMAND.DOWN);
-        assertEquals(map.getChickenPosition(), new Position(position2.getX(), position2.getY()+1));
-        Position position3 = map.getChickenPosition();
-        chickenController.execute(Gui.COMMAND.RIGHT);
-        assertEquals(map.getChickenPosition(), new Position(position3.getX()+1, position3.getY()));
-        Position position4 = map.getChickenPosition();
-        chickenController.execute(Gui.COMMAND.LEFT);
-        assertEquals(map.getChickenPosition(), new Position(position4.getX()-1, position4.getY()));
+        Chicken chicken = map.getChicken();
+        Position position1 = chicken.getPosition();
+        Mockito.when(gui.getNextCommand()).thenReturn(Gui.COMMAND.UP);
+        chickenController.execute();
+        assertEquals(chicken.getPosition(), new Position(position1.getX(), position1.getY()-1));
+        chicken.setPosition(new Position(chicken.getPosition().getX(), 0));
+        Position position2 = chicken.getPosition();
+        Mockito.when(gui.getNextCommand()).thenReturn(Gui.COMMAND.UP);
+        chickenController.execute();
+        assertEquals(chicken.getPosition(), position2);
+    }
 
-    }*/
+    @Test
+    public void DownTest() throws IOException {
+        GameMap map = new GameMap(40, 35);
+        Gui gui = Mockito.mock(Gui.class);
+        ChickenController chickenController = new ChickenController(gui, map);
+        Chicken chicken = map.getChicken();
+        Position position1 = chicken.getPosition();
+        Mockito.when(gui.getNextCommand()).thenReturn(Gui.COMMAND.DOWN);
+        chickenController.execute();
+        assertEquals(chicken.getPosition(), position1);
+        chicken.setPosition(new Position(chicken.getPosition().getX(), chicken.getPosition().getY()-4));
+        Position position2 = chicken.getPosition();
+        Mockito.when(gui.getNextCommand()).thenReturn(Gui.COMMAND.DOWN);
+        chickenController.execute();
+        assertEquals(chicken.getPosition(), new Position(position2.getX(), position2.getY()+1));
+    }
+
+    @Test
+    public void LeftTest() throws IOException {
+        GameMap map = new GameMap(40, 35);
+        Gui gui = Mockito.mock(Gui.class);
+        ChickenController chickenController = new ChickenController(gui, map);
+        Chicken chicken = map.getChicken();
+        Position position1 = chicken.getPosition();
+        Mockito.when(gui.getNextCommand()).thenReturn(Gui.COMMAND.LEFT);
+        chickenController.execute();
+        assertEquals(chicken.getPosition(), new Position(position1.getX()-1, position1.getY()));
+        chicken.setPosition(new Position(0, chicken.getPosition().getY()));
+        Position position2 = chicken.getPosition();
+        Mockito.when(gui.getNextCommand()).thenReturn(Gui.COMMAND.LEFT);
+        chickenController.execute();
+        assertEquals(chicken.getPosition(), position2);
+    }
+
+    @Test
+    public void RightTest() throws IOException {
+        GameMap map = new GameMap(40, 35);
+        Gui gui = Mockito.mock(Gui.class);
+        ChickenController chickenController = new ChickenController(gui, map);
+        Chicken chicken = map.getChicken();
+        Position position1 = chicken.getPosition();
+        Mockito.when(gui.getNextCommand()).thenReturn(Gui.COMMAND.RIGHT);
+        chickenController.execute();
+        assertEquals(chicken.getPosition(), new Position(position1.getX()+1, position1.getY()));
+        chicken.setPosition(new Position(map.getWidth(), chicken.getPosition().getY()));
+        Position position2 = chicken.getPosition();
+        Mockito.when(gui.getNextCommand()).thenReturn(Gui.COMMAND.RIGHT);
+        chickenController.execute();
+        assertEquals(chicken.getPosition(), position2);
+    }
+
+    @Test
+    public void checkVehicleCollisionTest(){
+        GameMap map = new GameMap(35, 40);
+        Gui gui = Mockito.mock(Gui.class);
+        ChickenController chickenController = new ChickenController(gui, map);
+        Chicken chicken = map.getChicken();
+        chicken.setPosition(new Position(1, 2));
+        int life = chicken.getLives();
+        Truck truck = new Truck(1, 2 ,"left");
+        map.addElement(truck);
+        chickenController.checkVehicleCollision(chicken.getPosition());
+        assertEquals(life - 1, chicken.getLives());
+        assertEquals(chicken.getPosition(), new Position(map.getWidth()/2, map.getHeight()-1));
+    }
+
+
+
+   @Test
+    public void checkCollisionTest(){
+        GameMap map = new GameMap(35, 40);
+        Gui gui = Mockito.mock(Gui.class);
+        ChickenController chickenController = new ChickenController(gui, map);
+        Chicken chicken = map.getChicken();
+        Coin coin = new Coin(1, 2, 3);
+        map.addElement(coin);
+        chicken.setPosition(new Position(1, 2));
+        int score = chicken.getScore();
+        chickenController.checkCollisions(chicken.getPosition());
+        assertEquals(score+coin.getValue(), chicken.getScore());
+        assertTrue(map.getCoins().isEmpty());
+
+    }
 }
--- src/test/java/crossyroads/controller/VehicleControllerTest.java
+++ src/test/java/crossyroads/controller/VehicleControllerTest.java
@@ -1,26 +1,47 @@
 package crossyroads.controller;
 
+import crossyroads.Game;
 import crossyroads.model.Car;
+import crossyroads.model.Chicken;
 import crossyroads.model.GameMap;
 import crossyroads.model.Position;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class VehicleControllerTest {
-    /*@Test
+    @Test
     public void moveVehicleTest(){
         GameMap map = new GameMap(40, 35);
         VehicleController vehicleController = new VehicleController(map);
         Car car1 = new Car(1, 2, "left");
         Car car2 = new Car(3, 6, "right");
+        Car car3 = new Car(map.getWidth()-1, 2, "right");
         map.addElement(car1);
         map.addElement(car2);
+        map.addElement(car3);
         Position position1 = car1.getPosition();
         Position position2 = car2.getPosition();
         vehicleController.moveVehicles();
         assertEquals(car1.getPosition(), new Position(position1.getX()-1, position1.getY()));
         assertEquals(car2.getPosition(), new Position(position2.getX()+1, position2.getY()));
+        assertEquals(car3.getPosition(), new Position(-car3.getLength(), car3.getPosition().getY()));
+    }
 
-    }*/
+    @Test
+    public void checkChickenCollisionTest(){
+        GameMap map = new GameMap(40, 35);
+        VehicleController vehicleController = new VehicleController(map);
+        Car car = new Car(1, 2, "right");
+        int life = map.getLives();
+        Chicken chicken = map.getChicken();
+        Position position = chicken.getPosition();
+        chicken.setPosition(new Position(1, 2));
+        vehicleController.checkChickenCollision(car);
+        assertEquals(life-1, chicken.getLives());
+        assertEquals(chicken.getPosition(), new Position(map.getWidth()/2, map.getHeight()-1));
+
+
+    }
 }
