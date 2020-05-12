a154eaae2b99263111a2611f2e03f0de43996326
anatcruz <anateresacruz2000@gmail.com>
2020-05-12 18:04:41 +0100

    Added tests

--- src/test/java/crossyroads/controller/VehicleControllerTest.java
+++ src/test/java/crossyroads/controller/VehicleControllerTest.java
@@ -1,9 +1,6 @@
 package crossyroads.controller;
 
-import crossyroads.model.Car;
-import crossyroads.model.Chicken;
-import crossyroads.model.GameMap;
-import crossyroads.model.Position;
+import crossyroads.model.*;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -16,15 +13,19 @@ public class VehicleControllerTest {
         Car car1 = new Car(1, 2, "left");
         Car car2 = new Car(3, 6, "right");
         Car car3 = new Car(map.getWidth()-1, 2, "right");
+        Car car4 = new Car(-1, 2, "left");
         map.addElement(car1);
         map.addElement(car2);
         map.addElement(car3);
+        map.addElement(car4);
         Position position1 = car1.getPosition();
         Position position2 = car2.getPosition();
         vehicleController.moveVehicles();
         assertEquals(car1.getPosition(), new Position(position1.getX()-1, position1.getY()));
         assertEquals(car2.getPosition(), new Position(position2.getX()+1, position2.getY()));
+        vehicleController.vehicleLeavesScreen(car4);
         assertEquals(car3.getPosition(), new Position(-car3.getLength(), car3.getPosition().getY()));
+        assertEquals(car4.getPosition(), new Position(map.getWidth(), car4.getPosition().getY()));
     }
 
     @Test
@@ -42,4 +43,5 @@ public class VehicleControllerTest {
 
 
     }
+
 }
