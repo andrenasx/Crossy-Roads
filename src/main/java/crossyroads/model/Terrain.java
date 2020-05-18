177f2fd418f86a41816e507352891e7d4830a068
andrenasx <andre2000.an@gmail.com>
2020-05-18 11:23:22 +0100

    Fixed not reading file last line, now not creating unnecessary Position

--- src/main/java/crossyroads/model/Terrain.java
+++ src/main/java/crossyroads/model/Terrain.java
@@ -36,7 +36,7 @@ public class Terrain {
             e.printStackTrace();
         }
         this.background = allLines.get(0);
-        for(int i = 1; i < allLines.size()-1; i++){
+        for(int i = 1; i < allLines.size(); i++){
             addElement(allLines.get(i));
         }
     }
@@ -52,33 +52,34 @@ public class Terrain {
     private void addElement(String line){
         String[] array_of_strings = line.split(",");
         String type = array_of_strings[0];
-        int x = Integer.valueOf(array_of_strings[1]);
-        int y = Integer.valueOf(array_of_strings[2]);
-        Position position = new Position(x, y);
+        int x = Integer.parseInt(array_of_strings[1]);
+        int y = Integer.parseInt(array_of_strings[2]);
         switch (type){
             case "C":
                 Car car;
                 if(array_of_strings[3].equals(">")){
-                    car = new Car(position.getX(), position.getY(), "right");
+                    car = new Car(x, y, "right");
                 }
                 else{
-                    car = new Car(position.getX(), position.getY(), "left");
+                    car = new Car(x, y, "left");
                 }
                 elements.add(car);
                 break;
             case "T":
                 Truck truck;
                 if(array_of_strings[3].equals(">")){
-                    truck = new Truck(position.getX(), position.getY(), "right");
+                    truck = new Truck(x, y, "right");
                 }
                 else{
-                    truck = new Truck(position.getX(), position.getY(), "left");
+                    truck = new Truck(x, y, "left");
                 }
                 elements.add(truck);
                 break;
             case "M":
-                Coin coin = new Coin(position.getX(), position.getY(), Integer.valueOf(array_of_strings[3]));
+                Coin coin = new Coin(x, y, Integer.parseInt(array_of_strings[3]));
                 elements.add(coin);
+                break;
+            default:
                 break;
         }
 
