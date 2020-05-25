e16e393f2dd58f6cbf142caa4f0c86108c81feb2
anatcruz <anateresacruz2000@gmail.com>
2020-05-25 19:33:26 +0100

    Changes the title's look and add another option: play

--- src/main/java/crossyroads/view/GuiHelpMenu.java
+++ src/main/java/crossyroads/view/GuiHelpMenu.java
@@ -6,9 +6,7 @@ import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.TerminalScreen;
-import crossyroads.model.GameModel;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,7 +14,7 @@ import java.util.List;
 
 public class GuiHelpMenu {
     private TerminalScreen screen;
-    public enum COMMAND {BACK, NOTHING, EOF};
+    public enum COMMAND {PLAY, BACK, NOTHING}
 
     public GuiHelpMenu() throws IOException {
         this.screen = ScreenFactory.getScreen();
@@ -25,7 +23,7 @@ public class GuiHelpMenu {
     public void draw() throws IOException {
         screen.clear();
         drawHelpMenu();
-        drawButton();
+        drawButtons();
         screen.refresh();
     }
 
@@ -38,52 +36,63 @@ public class GuiHelpMenu {
                 ' '
         );
         graphics.enableModifiers(SGR.BOLD);
-        graphics.putString(17, 5, "HELP");
-        List<String> instrutions = new ArrayList<>();
+        graphics.setForegroundColor(TextColor.Factory.fromString("#C8C8C8"));
+        graphics.putString(5, 2, "HH   HH  EEEEEE  LLL     PPPPP");
+        graphics.putString(5, 3, "HH   HH  EEE     LLL     PP   P");
+        graphics.putString(5, 4, "HHHHHHH  EEEEE   LLL     PPPPP");
+        graphics.putString(5, 5, "HH   HH  EEE     LLL     PP");
+        graphics.putString(5, 6, "HH   HH  EEEEEE  LLLLLL  PP");
+        List<String> instructions = new ArrayList<>();
         String inst1 = "-To move use the arrow of the keyboard";
-        instrutions.add(inst1);
+        instructions.add(inst1);
         String inst2 = "-To exit the game click ESCAPE";
-        instrutions.add(inst2);
+        instructions.add(inst2);
         String inst3 = "-Collect all the coins";
-        instrutions.add(inst3);
+        instructions.add(inst3);
         String inst4 = "-Reach the finish line on the top";
-        instrutions.add(inst4);
+        instructions.add(inst4);
         String inst5 = "-You lose health when hitting a vehicle";
-        instrutions.add(inst5);
+        instructions.add(inst5);
         String inst6 = "------------>Have fun!!!!!!<------------";
-        instrutions.add(inst6);
-        int row = 9;
-        for(int i = 0; i < instrutions.size(); i++){
-            graphics.putString(0, row, instrutions.get(i));
+        instructions.add(inst6);
+        int row = 11;
+        for (String instruction : instructions) {
+            graphics.putString(0, row, instruction);
             row += 2;
         }
 
     }
 
-    private void drawButton(){
+    private void drawButtons(){
         TextGraphics graphics = screen.newTextGraphics();
         graphics.setBackgroundColor(TextColor.Factory.fromString("#C8C8C8"));
-        graphics.fillRectangle(
-                    new TerminalPosition(29, 32),
-                    new TerminalSize(8, 2),
+        int column = 5;
+        for(int i = 0; i <= 2; i++){
+            graphics.fillRectangle(
+                    new TerminalPosition(column, 32),
+                    new TerminalSize(9, 2),
                     ' ');
-
+            column += 20;
+        }
         graphics.enableModifiers(SGR.BOLD);
         graphics.setForegroundColor(TextColor.Factory.fromString("#25221e"));
-        graphics.putString(31,32,"BACK");
-        graphics.putString(32,33,"[1]");
+        graphics.putString(26,32,"PLAY");
+        graphics.putString(27,33,"[2]");
+        graphics.putString(7, 32, "BACK");
+        graphics.putString(8, 33, "[1]");
+
     }
 
     public GuiHelpMenu.COMMAND getNextCommand() throws IOException {
-        KeyStroke input = screen.pollInput();
-        screen.readInput();
+        KeyStroke input = screen.readInput();
 
-        switch (input.getKeyType()){
-            case Character:
-                if(input.getCharacter() == '1')
-                    return COMMAND.BACK;
+        switch (input.getCharacter()){
+            case '1':
+                return COMMAND.BACK;
+            case '2':
+                return COMMAND.PLAY;
             default:
-                return GuiHelpMenu.COMMAND.NOTHING;
+                return COMMAND.NOTHING;
         }
     }
 }
