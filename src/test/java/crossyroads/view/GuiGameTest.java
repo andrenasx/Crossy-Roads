5992d72dd06a0e761575d876a5b434594c31a18a
anatcruz <anateresacruz2000@gmail.com>
2020-05-27 16:11:01 +0100

    Added tests

--- src/test/java/crossyroads/view/GuiGameTest.java
+++ src/test/java/crossyroads/view/GuiGameTest.java
@@ -1,18 +1,18 @@
 package crossyroads.view;
 
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import crossyroads.model.*;
 import org.junit.Test;
 import org.mockito.Mockito;
-import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
 
 public class GuiGameTest {
     @Test
@@ -23,22 +23,38 @@ public class GuiGameTest {
         TextGraphics graphics = mock(TextGraphics.class);
         when(screen.newTextGraphics()).thenReturn(graphics);
 
-        /*List<Element> elements = new ArrayList<>();
-        elements.add(new Chicken(20,20));
+        Chicken chicken = new Chicken(10, 10);
+        when(gameModel.getChicken()).thenReturn(chicken);
+        when(gameModel.getScore()).thenReturn(0);
+        when(gameModel.getLives()).thenReturn(3);
+        when(gameModel.getCurrentLevelInt()).thenReturn(1);
+        when(gameModel.getHeight()).thenReturn(35);
+
+        Level level = mock(Level.class);
+        when(gameModel.getCurrentLevel()).thenReturn(level);
+        when(level.getLevelBackground()).thenReturn("dgggggggggrrrggggggggggrrrggggggggg");
+
+        List<Element> elements = new ArrayList<>();
         elements.add(new Coin(15,20,1));
         elements.add(new Car(10,10,"left"));
         elements.add(new Truck(10,5,"right"));
-        when(level.getAllElements()).thenReturn(elements);*/
-        /*Chicken chicken = new Chicken(10, 10);
-        when(gameModel.getChicken()).thenReturn(chicken);
+        when(level.getAllElements()).thenReturn(elements);
 
         gui.draw();
 
-        verify(screen.newTextGraphics(),times(1)).putString(0,35, "Score: 10\tHealth: 3\tLevel: 1");
-        //verify(screen.newTextGraphics(),times(1)).putString(20,20, "O");
-        //verify(screen.newTextGraphics(),times(1)).putString(15,20, "O");
-        //verify(screen.newTextGraphics(),times(1)).putString(10,10, "<>");
-        //verify(screen.newTextGraphics(),times(1)).putString(10,5, "<==>");*/
+        verify(graphics,times(5)).enableModifiers(SGR.BOLD);
+        verify(screen.newTextGraphics(),times(1)).putString(0,35, "Score: 0\tHealth: 3\tLevel: 1");
+        verify(graphics,times(1)).setForegroundColor(TextColor.Factory.fromString("#FFFFFF"));
+        verify(graphics,times(30)).setBackgroundColor(TextColor.Factory.fromString("#006600"));
+        verify(screen.newTextGraphics(),times(1)).putString(10,10, "O");
+        verify(graphics, times(1)).setForegroundColor(TextColor.Factory.fromString("#FFFF00"));
+        verify(graphics, times(30)).setBackgroundColor(TextColor.Factory.fromString("#006600"));
+        verify(screen.newTextGraphics(),times(1)).putString(15,20, "O");
+        verify(graphics, times(1)).setForegroundColor(TextColor.Factory.fromString("#FF0000"));
+        verify(graphics,times(8)).setBackgroundColor(TextColor.Factory.fromString("#C8C8C8"));
+        verify(screen.newTextGraphics(),times(1)).putString(10,10, "<>");
+        verify(graphics, times(1)).setForegroundColor(TextColor.Factory.fromString("#0000FF"));
+        verify(screen.newTextGraphics(),times(1)).putString(10,5, "<==>");
     }
 
 }
